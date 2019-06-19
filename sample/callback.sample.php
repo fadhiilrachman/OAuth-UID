b20cf4a8a4f3429ce76400f4e143481d64b798b9
Fadhiil Rachman <fadhiilrachman@gmail.com>
2019-06-19 17:25:47 +0700

    Update callback.sample.php

--- sample/callback.sample.php
+++ sample/callback.sample.php
@@ -5,7 +5,7 @@ include 'config.sample.php';
 
 $uid = new Uid(UID_CLIENT_ID, UID_CLIENT_SECRET, UID_REDIRECT_URI);
 
-$uid->handleCallback($_GET);
+$uid->setScope(UID_SCOPES)->handleCallback($_GET);
 $selfInfo = $uid->getSelfInfo(
     array('email')
 );
